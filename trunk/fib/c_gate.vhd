16e8317edc356ff74903ea45c37bbfee6b2168df
wabsie@gmail.com <wabsie@gmail.com@5a7ad418-726d-68c8-78c6-6ee84100fe9c>
2011-04-11 07:15:11 +0000

    Working 3-stage pipeline with correct initialization.

--- trunk/fib/c_gate.vhd
+++ trunk/fib/c_gate.vhd
@@ -1,8 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use work.definitions.all;
 
 
 entity c_gate is
+	generic (
+		constant c_initial : std_logic
+	);
 	port (
 		a : in std_logic;
 		b : in std_logic;
@@ -17,8 +21,11 @@ architecture latch_implementation of c_gate is
 begin
 	set   <= a and b;	--   Set when a=1 and b=1
 	reset <= a nor b;	-- Reset when a=0 and b=0
-	
+		
 	latch : entity work.sr_latch(struct)
+	generic map(
+		q_init => c_initial
+	)
 	port map(
 		s => set,
 		r => reset,
--- trunk/fib/channel_latch.vhd
+++ trunk/fib/channel_latch.vhd
@@ -4,6 +4,10 @@ use work.definitions.all;
 
 
 entity channel_latch is
+	generic (
+		constant init_token : token_type := bubble;		-- Bubble
+		constant init_data : word_t := (others => 'U')	-- Unintialized
+	);
 	port (
 		left_in   : in channel_forward;
 		left_out  : out channel_backward;
@@ -14,10 +18,15 @@ end channel_latch;
 
 
 architecture struct of channel_latch is
-	signal lt_en : std_logic;	-- Latch enable
+	signal lt_en : latch_state;	-- Latch enable
+	signal data  : word_t := init_data;
 begin
+	right_out.data <= data;
 	
 	controller : entity work.latch_controller(simple)
+	generic map(
+		init_token => init_token
+	)
 	port map(
 		Rin  => left_in.req,
 		Aout => left_out.ack,
@@ -31,8 +40,8 @@ begin
 	-- Normal transparent latch, cf. figure 6.21 in S&F
 	process(left_in, lt_en)
 	begin
-		if (lt_en = '0') then
-			right_out.data <= left_in.data; -- Transparent
+		if (lt_en = follow) then
+			data <= transport left_in.data after delay; -- Transparent
 		end if;
 	end process;	
 
